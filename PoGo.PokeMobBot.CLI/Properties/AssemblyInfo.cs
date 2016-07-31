cb8b998c0c96b08b00a64b46ffdecb1876f9ad54
PocketMobsters <pitime2@outlook.com>
2016-07-30 20:08:11 -0500

    Modify Auto Updater

--- PoGo.PokeMobBot.CLI/Properties/AssemblyInfo.cs
+++ PoGo.PokeMobBot.CLI/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("NecroBot Client for Pokémon GO")]
+[assembly: AssemblyTitle("PokeMobBot Client for Pokémon GO")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
--- PoGo.PokeMobBot.Logic/State/VersionCheckState.cs
+++ PoGo.PokeMobBot.Logic/State/VersionCheckState.cs
@@ -61,7 +61,7 @@ namespace PoGo.PokeMobBot.Logic.State
                 Message = session.Translation.GetTranslation(TranslationString.DownloadingUpdate)
             });
             var remoteReleaseUrl =
-                $"https://github.com/NecronomiconCoding/NecroBot/releases/download/v{RemoteVersion}/";
+                $"https://github.com/PocketMobsters/PokeMobBot/releases/download/v{RemoteVersion}/";
             const string zipName = "Release.zip";
             var downloadLink = remoteReleaseUrl + zipName;
             var baseDir = Directory.GetCurrentDirectory();
