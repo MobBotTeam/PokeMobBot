e006b610b954fbaf7b454dac544ba979f2f2b7c7
DeadElle <solodeadelle@gmail.com>
2016-08-01 08:21:04 +0200

    PokemonsTransferFilter re-tweak v3

--- PoGo.PokeMobBot.Logic/Settings.cs
+++ PoGo.PokeMobBot.Logic/Settings.cs
@@ -150,7 +150,7 @@ namespace PoGo.PokeMobBot.Logic
         public bool TransferDuplicatePokemon = true;
         public bool PrioritizeIvOverCp = true;
         public int KeepMinCp = 1250;
-        public float KeepMinIvPercentage = 90;
+        public float KeepMinIvPercentage = 95;
         public int KeepMinDuplicatePokemon = 1;
         public bool KeepPokemonsThatCanEvolve = false;
 
@@ -328,7 +328,7 @@ namespace PoGo.PokeMobBot.Logic
 
         public Dictionary<PokemonId, TransferFilter> PokemonsTransferFilter = new Dictionary<PokemonId, TransferFilter>
         {
-            //criteria: based on NY Central Park and Tokyo variety + sniping optimization
+            //criteria: based on NY Central Park and Tokyo variety + sniping optimization v3
             {PokemonId.Venusaur, new TransferFilter(1500, 40, 1)},
             {PokemonId.Charizard, new TransferFilter(1500, 20, 1)},
             {PokemonId.Blastoise, new TransferFilter(1500, 20, 1)},
@@ -337,38 +337,35 @@ namespace PoGo.PokeMobBot.Logic
             {PokemonId.Clefable, new TransferFilter(1500, 60, 1)},
             {PokemonId.Vileplume, new TransferFilter(1750, 80, 1)},
             {PokemonId.Golduck, new TransferFilter(1750, 80, 1)},
-            {PokemonId.Arcanine, new TransferFilter(1750, 80, 1)},
+            {PokemonId.Arcanine, new TransferFilter(2000, 90, 1)},
             {PokemonId.Poliwrath, new TransferFilter(1500, 80, 1)},
             {PokemonId.Machamp, new TransferFilter(1250, 80, 1)},
             {PokemonId.Victreebel, new TransferFilter(1250, 60, 1)},
             {PokemonId.Golem, new TransferFilter(1500, 80, 1)},
             {PokemonId.Slowbro, new TransferFilter(1750, 80, 1)},
             {PokemonId.Farfetchd, new TransferFilter(1000, 90, 1)},
-            {PokemonId.Muk, new TransferFilter(1500, 80, 1)},
-            {PokemonId.Krabby, new TransferFilter(1500, 95, 1)},
-            {PokemonId.Exeggutor, new TransferFilter(2000, 90, 1)},
+            {PokemonId.Muk, new TransferFilter(2000, 80, 1)},
+            {PokemonId.Exeggutor, new TransferFilter(2250, 80, 1)},
             {PokemonId.Lickitung, new TransferFilter(1500, 80, 1)},
             {PokemonId.Chansey, new TransferFilter(1500, 95, 1)},
-            {PokemonId.Kangaskhan, new TransferFilter(1500, 40, 1)},
-            {PokemonId.Goldeen, new TransferFilter(1500, 95, 1)},
-            {PokemonId.Staryu, new TransferFilter(1500, 95, 1)},
+            {PokemonId.Kangaskhan, new TransferFilter(1500, 60, 1)},
             {PokemonId.MrMime, new TransferFilter(250, 40, 1)},
-            {PokemonId.Scyther, new TransferFilter(1500, 90, 1)},
+            {PokemonId.Scyther, new TransferFilter(1750, 90, 1)},
             {PokemonId.Jynx, new TransferFilter(1250, 90, 1)},
             {PokemonId.Electabuzz, new TransferFilter(1500, 80, 1)},
             {PokemonId.Magmar, new TransferFilter(1750, 80, 1)},
             {PokemonId.Pinsir, new TransferFilter(1750, 98, 1)},
             {PokemonId.Tauros, new TransferFilter(500, 90, 1)},
-            {PokemonId.Magikarp, new TransferFilter(1250, 95, 1)},
             {PokemonId.Gyarados, new TransferFilter(1750, 90, 1)},
-            {PokemonId.Lapras, new TransferFilter(1500, 80, 1)},
-            {PokemonId.Vaporeon, new TransferFilter(2000, 95, 1)},
+            {PokemonId.Lapras, new TransferFilter(2000, 90, 1)},
+            {PokemonId.Eevee, new TransferFilter(1500, 98, 1)},
+            {PokemonId.Vaporeon, new TransferFilter(2000, 98, 1)},
             {PokemonId.Jolteon, new TransferFilter(2000, 95, 1)},
             {PokemonId.Flareon, new TransferFilter(2000, 95, 1)},
-            {PokemonId.Porygon, new TransferFilter(1500, 90, 1)},
-            {PokemonId.Aerodactyl, new TransferFilter(1750, 80, 1)},
-            {PokemonId.Snorlax, new TransferFilter(2250, 95, 1)},
-            {PokemonId.Dragonite, new TransferFilter(1750, 90, 1)}
+            {PokemonId.Porygon, new TransferFilter(1500, 95, 1)},
+            {PokemonId.Aerodactyl, new TransferFilter(1750, 95, 1)},
+            {PokemonId.Snorlax, new TransferFilter(2500, 96, 1)},
+            {PokemonId.Dragonite, new TransferFilter(2500, 90, 1)}
         };
 
         public SnipeSettings PokemonToSnipe = new SnipeSettings
