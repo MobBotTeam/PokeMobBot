2081deb8c2beae5d15007774504b197099a8bd2f
Nicolas Schmitt <nicoschmitt@users.noreply.github.com>
2016-08-02 19:37:08 +0200

    Websocket - Evolve and Transfer (#296)
    * Add available candy as info on Pokemon List (to know if evolve is an option)
    
    * for event originated from a client respond only to him (no broadcast)
    
    * Handle Transfer and Evolve Pokemon events
    
    * Add ability to get pokemon settings from servers
    
    to know if a pokemon has an evolve state
    
    * fix to correct delay

---
+++ PoGo.PokeMobBot.Logic/Tasks/EvolveSpecificPokemonTask.cs
@@ -0,0 +1,35 @@
+﻿using PoGo.PokeMobBot.Logic.Event;
+using PoGo.PokeMobBot.Logic.State;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoGo.PokeMobBot.Logic.Tasks
+{
+    public class EvolveSpecificPokemonTask
+    {
+        public static async Task Execute(ISession session, string pokemonId)
+        {
+            var id = ulong.Parse(pokemonId);
+
+            var all = await session.Inventory.GetPokemons();
+            var pokemons = all.OrderByDescending(x => x.Cp).ThenBy(n => n.StaminaMax);
+            var pokemon = pokemons.FirstOrDefault(p => p.Id == id);
+
+            if (pokemon == null) return;
+
+            var evolveResponse = await session.Client.Inventory.EvolvePokemon(pokemon.Id);
+
+            session.EventDispatcher.Send(new PokemonEvolveEvent
+            {
+                Id = pokemon.PokemonId,
+                Exp = evolveResponse.ExperienceAwarded,
+                Result = evolveResponse.Result
+            });
+
+            await Task.Delay(session.LogicSettings.DelayEvolvePokemon);
+        }
+    }
+}
