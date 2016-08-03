82f4513fd6544434c2404bb8c9145f9ee5775b90
PocketMobsters <pitime2@outlook.com>
2016-08-03 13:48:31 -0500

    Quick teleport logic change

--- PoGo.PokeMobBot.Logic/Tasks/FarmPokestopsTask.cs
+++ PoGo.PokeMobBot.Logic/Tasks/FarmPokestopsTask.cs
@@ -448,7 +448,7 @@ namespace PoGo.PokeMobBot.Logic.Tasks
                             i.CooldownCompleteTimestampMs < DateTime.UtcNow.ToUnixTime() &&
                             ( // Make sure PokeStop is within max travel distance, unless it's set to 0.
                                 LocationUtils.CalculateDistanceInMeters(
-                                    session.Settings.DefaultLatitude, session.Settings.DefaultLongitude,
+                                    session.Client.CurrentLatitude, session.Client.CurrentLongitude,
                                     i.Latitude, i.Longitude) < session.LogicSettings.MaxTravelDistanceInMeters) ||
                             session.LogicSettings.MaxTravelDistanceInMeters == 0
                     );
